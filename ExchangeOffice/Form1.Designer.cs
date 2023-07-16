987df4d124c4825b8b769382584af5715efd7dc3
omerfdev <omerfdev@gmail.com>
2023-07-16 22:52:22 +0300

    Add project files.

---
+++ ExchangeOffice/Form1.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace ExchangeOffice
+{
+	partial class Form1
+	{
+		/// <summary>
+		///  Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		///  Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		///  Required method for Designer support - do not modify
+		///  the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+			lblExchangeOffice = new Label();
+			lblValue = new Label();
+			lblCurrency = new Label();
+			SuspendLayout();
+			// 
+			// lblExchangeOffice
+			// 
+			lblExchangeOffice.AutoSize = true;
+			lblExchangeOffice.Location = new Point(21, 19);
+			lblExchangeOffice.Name = "lblExchangeOffice";
+			lblExchangeOffice.Size = new Size(0, 20);
+			lblExchangeOffice.TabIndex = 0;
+			// 
+			// lblValue
+			// 
+			lblValue.AutoSize = true;
+			lblValue.Location = new Point(422, 19);
+			lblValue.Name = "lblValue";
+			lblValue.Size = new Size(0, 20);
+			lblValue.TabIndex = 1;
+			// 
+			// lblCurrency
+			// 
+			lblCurrency.AutoSize = true;
+			lblCurrency.Location = new Point(213, 19);
+			lblCurrency.Name = "lblCurrency";
+			lblCurrency.Size = new Size(0, 20);
+			lblCurrency.TabIndex = 2;
+			// 
+			// Form1
+			// 
+			AutoScaleDimensions = new SizeF(8F, 20F);
+			AutoScaleMode = AutoScaleMode.Font;
+			ClientSize = new Size(800, 450);
+			Controls.Add(lblCurrency);
+			Controls.Add(lblValue);
+			Controls.Add(lblExchangeOffice);
+			Icon = (Icon)resources.GetObject("$this.Icon");
+			Name = "Form1";
+			Text = "Exchange Office";
+			Load += Form1_Load;
+			ResumeLayout(false);
+			PerformLayout();
+		}
+
+		#endregion
+
+		private Label lblExchangeOffice;
+		private Label lblValue;
+		private Label lblCurrency;
+	}
+}
---
+++ ExchangeOffice/Form1.cs
@@ -0,0 +1,50 @@
+using System.Text.Json;
+
+namespace ExchangeOffice
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private async void Form1_Load(object sender, EventArgs e)
+		{
+			while (true) { await GetCurrency(); }
+
+		}
+
+		private async Task GetCurrency()
+		{
+			string[] moneyType = { "EUR", "USD", "GBP", "AUD", "CHF", "JPY", "SAR" };
+			using (HttpClient client = new HttpClient())
+			{
+				foreach (string money in moneyType)
+				{
+					string url = "http://dovizkurlari-l6vtviaacq-uc.a.run.app/api/doviz/" + money;
+					HttpResponseMessage response = await client.GetAsync(url);
+					if (response.IsSuccessStatusCode)
+					{
+						string jsonResponse = await response.Content.ReadAsStringAsync();
+						Currency currency = JsonSerializer.Deserialize<Currency>(jsonResponse);
+						DateTime currentTime = DateTime.Now;
+						lblExchangeOffice.Text = currentTime.ToString();
+						lblCurrency.Text = currency.CurrencyName;
+						lblValue.Text = currency.ForexBuying;
+					}
+					else
+					{
+						MessageBox.Show($"{response.StatusCode}");
+					}
+				}
+			}
+		}
+
+		public class Currency
+		{
+			public string CurrencyName { get; set; }
+			public string ForexBuying { get; set; }
+		}
+	}
+}
